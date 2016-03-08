9cf8b2b46eac069c1dbd27532e75905d4478eef5
ShiftUnion <nick.cahill@gmail.com>
2016-03-09 11:47:52 +1300

    Added is bower dependencies (and Git ignore), removing CDN dependencies

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
 app.get("/", function(req,res){
     res.send("Hello books");
 })
+
+
+
 
 var port = 5000;
 app.listen(port, function (err) {
