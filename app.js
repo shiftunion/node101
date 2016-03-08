a036e78e07f9815160ae79148ed33b8a0c4046ad
ShiftUnion <nick.cahill@gmail.com>
2016-03-09 11:03:41 +1300

    Pulled in some external resources

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-
 var express = require('express');
 
-var myString;
-myString = "nice man";
-///
-console.log(myString);
+var app = express();
 
+app.get("/", function(req,res){
+    res.send("Hello books");
+})
 
+var port = 5000;
+app.listen(port, function (err) {
+    console.log('Running server on port ' + port);
+});
