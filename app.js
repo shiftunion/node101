53772dbc0b412c1a838e4c4961be6d25de43954b
ShiftUnion <nick.cahill@gmail.com>
2016-03-09 17:01:14 +1300

    Added EJS as the final templating language example
    The next commit will cleanup handlebars and jade

--- app.js
+++ app.js
@@ -7,9 +7,7 @@
     app.use(express.static('public'));
     app.set('views', './src/views');
 
-    var handlebars = require('express-handlebars');
-    app.engine('.hbs', handlebars({extname: '.hbs'}));
-    app.set('view engine', '.hbs');
+    app.set('view engine', 'ejs');
 
     app.get('/', function (req, res) {
         res.render('index', {
