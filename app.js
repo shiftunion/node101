f69bc6c397ffb11020efa0c4933025b356071d14
ShiftUnion <nick.cahill@gmail.com>
2016-03-10 13:58:06 +1300

    Set up some book routes, and a book view

--- app.js
+++ app.js
@@ -4,10 +4,69 @@
 
     var app = express();
 
+
     app.use(express.static('public'));
     app.set('views', './src/views');
 
     app.set('view engine', 'ejs');
+
+
+    var books = [
+        {
+            "title": "Dude, where is my Json",
+            "genre": "Java",
+            "read": "true",
+            "author": "Herbert Schildt"
+        },
+        {
+            "title": "Magic, shit you dont need right now",
+            "genre": "C++",
+            "read": "false",
+            "author": "E.Balagurusamy"
+        },
+        {
+            "title": "Cats, when did you eat one last",
+            "genre": "C#",
+            "read": "true",
+            "author": "John Burner"
+        },
+        {
+            "title": "Mike, the brother you did not want",
+            "genre": "JavaScript",
+            "read": "false",
+            "author": "Nick Cahill"
+        },
+        {
+            "title": "Amazing things you didn't know united",
+            "genre": "Java",
+            "read": "false",
+            "author": "Cat a Moran"
+        },
+    ];
+
+    var bookRouter = express.Router();
+    bookRouter.route('/')
+        .get(function (req, res) {
+            res.render('books', {
+                    nav: [{
+                        Link: '/Books',
+                        Text: 'Books'
+                    },
+                        {
+                            Link: '/Authors',
+                            Text: 'Authors'
+                        }],
+                    books: books
+                });
+        });
+
+    bookRouter.route('/Single')
+        .get(function (req, res) {
+            res.send('hello Single nick\'s books');
+        });
+
+
+    app.use('/Books', bookRouter);
 
     app.get('/', function (req, res) {
         res.render('index', {
